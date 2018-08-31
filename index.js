394760231676cc156250ba3d746204be32553ad9
Sassdesu <anas.bassoumi@gmail.com>
2018-08-31 23:05:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,23 +13,12 @@ const config = require("./config.json");
 
 //
 const { Client, Util } = require('discord.js');
-const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
 const youtube = new YouTube(config.GOOGLE_API_KEY);
 
 const queue = new Map();
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-client.on('ready', () => console.log('Yo this ready!'));
-
-client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
-
-client.on('reconnecting', () => console.log('I am reconnecting now!'));
 
 //
 
