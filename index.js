fc4cf3c7307fcbfd2d627e6114fafd28b14e8253
Sassdesu <anas.bassoumi@gmail.com>
2018-09-01 00:37:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -118,20 +118,20 @@ client.on("guildCreate", guild => {
      // get the delete count, as an actual number.
      const deleteCount = parseInt(args[0], 10);
      
-     // Ooooh nice, combined conditions. <3
-     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-     
-     // So we get our messages, and delete them. Simple enough, right?
-     const fetched = await message.channel.fetchMessages({limit: deleteCount});
-     message.channel.bulkDelete(fetched)
-       .catch(error => message.reply('Couldn't delete messages because of: ${error}'));
-   }
- 
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+
   if(command === "sanyu") {
     message.reply(`Check sanyu.ml uwu`);
   }
-  
+
 });
 
 client.login(config.token);
